0e8292f0c3f20e08a3c5f07f91bc182212df679f
TTRider <software@ttrider.com>
2020-09-23 23:22:02 -0700

    parser fun

--- src/parser.ts
+++ src/parser.ts
@@ -9,6 +9,14 @@ interface ParserContext extends ParserSource {
 
 }
 
+interface Shape {
+
+    parent?: Shape;
+    leftToken: number;
+    leftTokenEndPos: number;
+
+}
+
 export function parser(source: string | ParserSource) {
 
     const context: ParserContext = (typeof source === "string") ? { source } : source;
@@ -20,74 +28,66 @@ export function parser(source: string | ParserSource) {
 
 function collectStartTokens(context: ParserContext) {
 
-    // start token markers
-    // 2 - [
-    // 3 - (
-    // 4 - /
-    // 5 - {
-    // 6 - <
-    // 7 - |
-    // 8 - <[
-    // 9 - <|
-    // 10 - [| 
-    // 11 - [||
+    const excludeRegions = processExcludeRegions();
 
-    const startTokenRegex = /(^\s*((\[)|(\()|(\/)|(\{)|(\<)|(\|)|(\<\[)|(\<\|)|(\[\|)|(\[\|\|))\s+)/gm;
+    const regex = new RegExp(pattern, "gm");
 
-    // end token markers
-    // 2 - ]
-    // 3 - (
-    // 4 - \
-    // 5 - {
-    // 6 - >
-    // 7 - |
-    // 8 - ]>
-    // 9 - |>
-    // 10 - |] 
-    // 11 - ||]
-    // 12 - ()
-    // 13 - ])
-    // 14 - }}
+    let state = 0;
+    let current: Shape | undefined;
 
-    const endTokenRegex = /\s+((\])|(\))|(\\)|(\})|(\>)|(\|)|(\]\<)|(\|\>)|(\]\>)|(\|\])|(\|\|\])|(\(\))|(\]\))|(\}\}))/gm;
-
-    // comment blocks
-
-    const commentTokenRegex = /(\/\/.*)|(```)/gm;
-
-
-
-
-    const startTokens = getMarkers(startTokenRegex);
-    const endTokens = getMarkers(endTokenRegex);
-    const commentTokens = getMarkers(commentTokenRegex, 1);
-
-    // post process comment tokens
-
-
-    function getMarkers(regExp: RegExp, startIndex = 2) {
-        const markers: { index: number; length: number; token: number }[] = [];
-        let m: RegExpExecArray | null;
-        while ((m = regExp.exec(context.source)) !== null) {
-            // This is necessary to avoid infinite loops with zero-width matches
-            if (m.index === regExp.lastIndex) {
-                regExp.lastIndex++;
-            }
-            for (let index = startIndex; index < m.length; index++) {
-                if (m[index]) {
-                    markers.push({
-                        index: m.index,
-                        length: m.length,
-                        token: index
-                    });
-                    break;
-                }
-            }
+    let m: RegExpExecArray | null;
+    while ((m = regex.exec(context.source)) !== null) {
+        // This is necessary to avoid infinite loops with zero-width matches
+        if (m.index === regex.lastIndex) {
+            regex.lastIndex++;
         }
-        return markers;
+
+
+
+        processMatch(m);
+
+
+
+
+
+        leftTokenmap
+
+
+
+
     }
 
-    function getCommentRegions(regExp: RegExp) {
+    function processMatch(m: RegExpExecArray) {
+        const index = m.index;
+        const length = m.length;
+
+        // validate position
+        // TODO: need more efficient algorythm here
+        if (excludeRegions.findIndex(item => item.start <= index && item.end > index) !== -1) {
+            return;
+        }
+
+        if (m[inlineCommentIndex]) {
+            // found another comment;
+            excludeRegions.push({ start: m.index, end: m.index + m.length });
+            return;
+        }
+
+        for (let i = leftTokenStartIndex; i < leftTokenEndIndex; i++) {
+            if (m[i]) {
+                if (!current) {
+                    current = { leftToken: i, leftTokenEndPos: index + length };
+                } else {
+                    current = { parent: current, leftToken: i, leftTokenEndPos: index + length };
+                }
+                return;
+            }
+        }
+    }
+
+
+    function processExcludeRegions() {
+        const excludeRegionRegex = /(^\s*```)|(```.*?```)/gm;
 
         const markers: { start: number; end: number }[] = [];
 
@@ -95,13 +95,13 @@ function collectStartTokens(context: ParserContext) {
         let startRegion = -1;
 
         let m: RegExpExecArray | null;
-        while ((m = regExp.exec(context.source)) !== null) {
+        while ((m = excludeRegionRegex.exec(context.source)) !== null) {
             // This is necessary to avoid infinite loops with zero-width matches
-            if (m.index === regExp.lastIndex) {
-                regExp.lastIndex++;
+            if (m.index === excludeRegionRegex.lastIndex) {
+                excludeRegionRegex.lastIndex++;
             }
 
-            if (m[2]) { //found ```
+            if (m[1]) { //found standalone ```
                 switch (state) {
                     case 0:
                         state = 1; // mark start region
@@ -112,130 +112,18 @@ function collectStartTokens(context: ParserContext) {
                         markers.push({ start: startRegion, end: m.index + m.length });
                         break;
                 }
-            } else if (m[1]) {
-                if (state === 0) // only if not inside the ```
+            } else if (m[2]) {
+                if (state === 0) // only if not inside the block ```
                 {
                     markers.push({ start: m.index, end: m.index + m.length });
                 }
             }
         }
-    }
 
-
-}
-
-
-function collectStartTokens2(context: ParserContext) {
-
-
-    // detect ``` blocks first
-
-
-
-
-    // start token markers
-    // 2 - [
-    // 3 - (
-    // 4 - /
-    // 5 - {
-    // 6 - <
-    // 7 - |
-    // 8 - <[
-    // 9 - <|
-    // 10 - [| 
-    // 11 - [||
-
-    const startTokenRegex = new RegExp("^\\s*((\\[)|(\\()|(\\/)|(\\{)|(\\<)|(\\|)|(\\<\\[)|(\\<\\|)|(\\[\\|)|(\\[\\|\\|))\\s+", "gm");
-
-    // end token markers
-    // 2 - ]
-    // 3 - (
-    // 4 - \
-    // 5 - {
-    // 6 - >
-    // 7 - |
-    // 8 - ]>
-    // 9 - |>
-    // 10 - |] 
-    // 11 - ||]
-    // 12 - ()
-    // 13 - ])
-    // 14 - }}
-
-    const endTokenRegex = new RegExp("\\s+((\\])|(\\))|(\\\\)|(\\})|(\\>)|(\\|)|(\\]\\<)|(\\|\\>)|(\\]\\>)|(\\|\\])|(\\|\\|\\])|(\\(\\))|(\\]\\))|(\\}\\}))", "gm");
-
-    // comment blocks
-
-    const commentTokenRegex = new RegExp("(\\/\\/.*)|(```)", "gm");
-
-
-
-
-    const startTokens = getMarkers(startTokenRegex);
-    const endTokens = getMarkers(endTokenRegex);
-    const commentTokens = getMarkers(commentTokenRegex, 1);
-
-    // post process comment tokens
-
-
-    function getMarkers(regExp: RegExp, startIndex = 2) {
-        const markers: { index: number; length: number; token: number }[] = [];
-        let m: RegExpExecArray | null;
-        while ((m = regExp.exec(context.source)) !== null) {
-            // This is necessary to avoid infinite loops with zero-width matches
-            if (m.index === regExp.lastIndex) {
-                regExp.lastIndex++;
-            }
-            for (let index = startIndex; index < m.length; index++) {
-                if (m[index]) {
-                    markers.push({
-                        index: m.index,
-                        length: m.length,
-                        token: index
-                    });
-                    break;
-                }
-            }
-        }
         return markers;
     }
-
-    function getCommentRegions(regExp: RegExp) {
-
-        const markers: { start: number; end: number }[] = [];
-
-        let state = 0;
-        let startRegion = -1;
-
-        let m: RegExpExecArray | null;
-        while ((m = regExp.exec(context.source)) !== null) {
-            // This is necessary to avoid infinite loops with zero-width matches
-            if (m.index === regExp.lastIndex) {
-                regExp.lastIndex++;
-            }
-
-            if (m[2]) { //found ```
-                switch (state) {
-                    case 0:
-                        state = 1; // mark start region
-                        startRegion = m.index;
-                        break;
-                    case 1:
-                        state = 0; // mark end regoin
-                        markers.push({ start: startRegion, end: m.index + m.length });
-                        break;
-                }
-            } else if (m[1]) {
-                if (state === 0) // only if not inside the ```
-                {
-                    markers.push({ start: m.index, end: m.index + m.length });
-                }
-            }
-        }
-    }
-
-
 }
+
 
 
 
@@ -243,7 +131,7 @@ export default parser;
 
 const comentToken = "(\\/\\/.*)";
 
-const startTokenmap = [
+const leftTokenmap = [
     "[",
     "(",
     "/",
@@ -256,7 +144,7 @@ const startTokenmap = [
     "[||"
 ];
 
-const endTokenmap = [
+const rightTokenmap = [
     "]",
     "(",
     "\\",
@@ -273,16 +161,20 @@ const endTokenmap = [
 ];
 
 const inlineCommentIndex = 1;
-const startTokensIndex = 5;
-const endTokenIndex = startTokensIndex + startTokenmap.length + 2; //16
-const idTokenIndex = endTokenIndex + endTokenmap.length + 2;
+const leftTokenStartIndex = 5;
+const leftTokenEndIndex = leftTokenStartIndex + leftTokenmap.length;
+
+const rightTokenStartIndex = leftTokenEndIndex + 2; //16
+const rightTokenEndIndex = rightTokenStartIndex + rightTokenmap.length;
+
+const idTokenIndex = rightTokenEndIndex + 2;
 const styleTokenIndex = idTokenIndex + 1;
 
 const pattern =
     "(\\/\\/.*)|(^\\s*("
-    + startTokenmap.map(t => "(" + t.replace(/./gm, (v) => "\\" + v) + ")").join("|")
+    + leftTokenmap.map(t => "(" + t.replace(/./gm, (v) => "\\" + v) + ")").join("|")
     + ")\\s+)|(\\s+("
-    + endTokenmap.map(t => "(" + t.replace(/./gm, (v) => "\\" + v) + ")").join("|")
+    + rightTokenmap.map(t => "(" + t.replace(/./gm, (v) => "\\" + v) + ")").join("|")
     + ")(:[a-zA-Z][a-zA-Z0-9_]*)?(@[a-zA-Z][a-zA-Z0-9_]*)?\\s)";
 
 
